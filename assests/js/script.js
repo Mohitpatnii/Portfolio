83624c0f8302a99a873d25cab7c2612197d60216
Mohitpatnii <88242171+Mohitpatnii@users.noreply.github.com>
2021-08-01 11:40:22 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Competitive Coding", "Marketing Strategist", "Product Designing", "UI/UX Research"],
+        strings: ["Competitive Coding", "Marketing Strategy", "Product Designing", "UI/UX Research"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
